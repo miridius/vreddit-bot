d19f0c461b32599759b3a4a5dc7fb1c11f78ae22
David Rolle <miridius@miridi.us>
2021-01-17 02:10:51 +0100

    Add support for inline queries

--- src/handler/index.js
+++ src/handler/index.js
@@ -10,31 +10,37 @@ const {
   sendVideo,
 } = require('./utils');
 
-/**
- * @type import('serverless-telegram').MessageHandler
- */
-module.exports = async ({ text, chat: { id, type }, message_id }, log) => {
-  log.verbose('Running on', OS_INFO);
+const CACHE_CHAT = {
+  id: -375023585,
+  title: 'V.redd.it Video Cache',
+  type: 'group',
+  all_members_are_administrators: true,
+};
 
-  // Check message for a v.redd.it link
+const parseText = (text, log) => {
   const [url, videoId] =
     text?.match(/https?:\/\/v\.redd\.it\/([-a-zA-Z0-9]+)/) || [];
   log.verbose({ url, videoId });
-  if (!url) return;
+  return { url, videoId };
+};
 
-  // Get local paths for temp video file and persisted json metadata file
-  const { videoFile, metadataFile } = getFilePaths(videoId);
-
-  // Check if we can re-use an existing file
+const getCachedFileId = (videoId, log) => {
+  const { metadataFile } = getFilePaths(videoId);
   let fileId = fs.existsSync(metadataFile) && require(metadataFile)?.fileId;
   if (fileId) {
-    log.info('Re-sending existing telegram file ID:', fileId);
-    return { video: fileId, reply_to_message_id: message_id };
+    log.info('Found existing telegram file ID:', fileId);
+    return fileId;
   }
+};
 
-  // Inform the users that the work is in progress since it might take a while
-  // NOTE: we don't wait for this to complete, just fire it and let it run
-  telegramApiCall(`sendChatAction?chat_id=${id}&action=upload_video`);
+const downloadAndSendVideo = async (
+  url,
+  videoId,
+  { id, type },
+  replyTo,
+  log
+) => {
+  const { videoFile, metadataFile } = getFilePaths(videoId);
 
   // If the video exists, it might be from a failed previous execution
   if (fs.existsSync(videoFile)) {
@@ -54,14 +60,64 @@ module.exports = async ({ text, chat: { id, type }, message_id }, log) => {
   if (size > MAX_FILE_SIZE_BYTES) {
     const text = `Video too large (${(size / 1024 / 1024).toFixed(2)} MB) :(`;
     log.error(text);
-    return type === 'private' && { reply_to_message_id: message_id, text };
+    if (type === 'private') {
+      await telegramApiCall(
+        `sendMessage?chat_id=${id}&text=${text}$reply_to_message_id=${replyTo}`
+      );
+    }
+    return;
   }
   log.verbose({ size });
 
   // Create the sendVideo method form data
-  const form = createForm(id, videoFile, size, width, height, message_id);
+  const form = createForm(id, videoFile, size, width, height, replyTo);
 
   // Send the video to telegram
-  fileId = await sendVideo(form, log);
+  const fileId = await sendVideo(form, log);
   if (fileId) fs.writeFileSync(metadataFile, JSON.stringify({ fileId }));
+  return fileId;
+};
+
+/**
+ * @type import('serverless-telegram').MessageHandler
+ */
+exports.message = async ({ text, chat, message_id }, log) => {
+  log.verbose('Running on', OS_INFO);
+
+  // Check message for a v.redd.it link
+  const { url, videoId } = parseText(text, log);
+  if (!url) return;
+
+  // Check if we can re-use an existing file
+  let fileId = getCachedFileId(videoId, log);
+  if (fileId) return { video: fileId, reply_to_message_id: message_id };
+
+  // Inform the users that the work is in progress since it might take a while
+  // NOTE: we don't wait for this to complete, just fire it and let it run
+  telegramApiCall(`sendChatAction?chat_id=${chat.id}&action=upload_video`);
+
+  // Download and send the file
+  await downloadAndSendVideo(url, videoId, chat, message_id, log);
+};
+
+/**
+ * @type import('serverless-telegram').InlineHandler
+ */
+exports.inline = async ({ query }, log) => {
+  // End early if query is empty
+  if (query === '') return;
+
+  // Check message for a v.redd.it link
+  const { url, videoId } = parseText(query, log);
+  if (!url) return;
+
+  // Check if we can re-use an existing file, otherwise upload it to CACHE_CHAT
+  const fileId =
+    getCachedFileId(videoId, log) ||
+    (await downloadAndSendVideo(url, videoId, CACHE_CHAT, undefined, log));
+
+  // Send the results list
+  if (fileId) {
+    return [{ title: `Send video (${videoId}.mp4)`, video_file_id: fileId }];
+  }
 };
--- test/handler.test.js
+++ test/handler.test.js
@@ -1,6 +1,6 @@
 //@ts-check
 const { CHAT_ID, log, mocked } = require('./helpers');
-const handler = require('../src/handler');
+const { message, inline } = require('../src/handler');
 jest.mock('../src/handler/utils');
 const {
   getFilePaths,
@@ -11,7 +11,7 @@ const {
   telegramApiCall,
 } = mocked(require('../src/handler/utils'));
 
-const { existsSync, unlinkSync, copyFileSync } = require('fs');
+const { existsSync, unlinkSync, copyFileSync, writeFileSync } = require('fs');
 const os = require('os');
 const FormData = require('form-data');
 
@@ -22,19 +22,28 @@ const getTestDataPaths = (videoId) => ({
   videoFile: `${testDataDir}/${videoId}.mp4`,
   metadataFile: `${testDataDir}/${videoId}.json`,
 });
+getFilePaths.mockImplementation(getTestDataPaths);
 
 const deleteIfExisting = (file) => existsSync(file) && unlinkSync(file);
 
-describe('handler', () => {
+describe('handler.message', () => {
   const message_id = 3;
   const msgReply = (text, type) =>
     // @ts-ignore
-    handler({ text, chat: { id: CHAT_ID, type }, message_id }, log);
+    message({ text, chat: { id: CHAT_ID, type }, message_id }, log);
 
   it('ignores messages without v.redd.it links', async () => {
     expect(await msgReply('foo')).toBeUndefined();
     expect(await msgReply('https://example.com/foo')).toBeUndefined();
     expect(await msgReply('https://v.redd.it')).toBeUndefined();
+  });
+
+  it('re-uses an existing file ID', async () => {
+    expect(await msgReply('...https://v.redd.it/hf352syjjka61/blah')).toEqual({
+      reply_to_message_id: 3,
+      video:
+        'BAACAgIAAxkDAAPdX_7_uS4ZUhWLumHZqKJOSPgWFhsAAscLAAIBhPlLfmhpHOQ0Tt8eBA',
+    });
   });
 
   it('downloads and sends a new video, saving the file ID', async () => {
@@ -47,7 +56,6 @@ describe('handler', () => {
     form.append('a', 1);
     const size = 35;
 
-    getFilePaths.mockReturnValueOnce({ videoFile, metadataFile });
     downloadVideo.mockImplementationOnce((_, outputFile) => {
       copyFileSync(outputFile + '.txt', outputFile);
       return Promise.resolve('ffmpeg stderr');
@@ -80,13 +88,72 @@ describe('handler', () => {
     );
     expect(sendVideo).toHaveBeenLastCalledWith(form, log);
   });
+});
+
+describe('handler.inline', () => {
+  // @ts-ignore
+  const inlineReply = (query) => inline({ query }, log);
+
+  it('ignores messages without v.redd.it links', async () => {
+    expect(await inlineReply('')).toBeUndefined();
+    expect(await inlineReply('foo')).toBeUndefined();
+    expect(await inlineReply('https://example.com/foo')).toBeUndefined();
+    expect(await inlineReply('https://v.redd.it')).toBeUndefined();
+  });
 
   it('re-uses an existing file ID', async () => {
-    getFilePaths.mockImplementationOnce(getTestDataPaths);
-    expect(await msgReply('...https://v.redd.it/hf352syjjka61/blah')).toEqual({
-      reply_to_message_id: 3,
-      video:
-        'BAACAgIAAxkDAAPdX_7_uS4ZUhWLumHZqKJOSPgWFhsAAscLAAIBhPlLfmhpHOQ0Tt8eBA',
+    expect(
+      await inlineReply('...https://v.redd.it/hf352syjjka61/blah')
+    ).toEqual([
+      {
+        title: 'Send video (hf352syjjka61.mp4)',
+        video_file_id:
+          'BAACAgIAAxkDAAPdX_7_uS4ZUhWLumHZqKJOSPgWFhsAAscLAAIBhPlLfmhpHOQ0Tt8eBA',
+      },
+    ]);
+  });
+
+  it('downloads and sends a new video, saving the file ID', async () => {
+    const videoId = 'inline-video-id';
+    const url = `https://v.redd.it/${videoId}`;
+    const { videoFile, metadataFile } = getTestDataPaths(videoId);
+    const width = 111;
+    const height = 222;
+    const form = new FormData();
+    form.append('a', 2);
+    const size = 10;
+
+    downloadVideo.mockImplementationOnce((_, outputFile) => {
+      writeFileSync(outputFile, 'dummy data');
+      return Promise.resolve('inline stderr');
     });
+    getOutputDimensions.mockReturnValueOnce({ width, height });
+    createForm.mockReturnValueOnce(form);
+    sendVideo.mockReturnValueOnce(Promise.resolve('new file ID'));
+
+    deleteIfExisting(videoFile);
+    deleteIfExisting(metadataFile);
+    expect(await inlineReply(url)).toEqual([
+      {
+        title: `Send video (${videoId}.mp4)`,
+        video_file_id: 'new file ID',
+      },
+    ]);
+    expect(existsSync(metadataFile)).toBe(true);
+    expect(require(metadataFile)).toEqual({ fileId: 'new file ID' });
+    deleteIfExisting(metadataFile);
+
+    expect(getFilePaths).toHaveBeenLastCalledWith(videoId);
+    expect(downloadVideo).toHaveBeenLastCalledWith(url, videoFile, log);
+    expect(getOutputDimensions).toHaveBeenLastCalledWith('inline stderr', log);
+    expect(createForm).toHaveBeenLastCalledWith(
+      -375023585,
+      videoFile,
+      size,
+      width,
+      height,
+      undefined
+    );
+    expect(sendVideo).toHaveBeenLastCalledWith(form, log);
   });
 });
